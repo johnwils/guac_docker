d224a9f9bc608737378f71244558ef47fc726110
John Wilson <johnner@me.com>
2024-07-30 14:02:02 -0700

    update config

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const express = require("express");
-const http = require("http");
 const GuacamoleLite = require("guacamole-lite");
 
-const HTTP_PORT = 8080;
-
-const app = express();
-const server = http.createServer(app);
-
 const guacdOptions = {
-  host: process.env.GUACD_HOST || "guacd",
-  port: parseInt(process.env.GUACD_PORT, 10) || 4822,
+  host: process.env.GUACD_HOST,
+  port: process.env.GUACD_PORT,
 };
-
-console.log(guacdOptions);
 
 const clientOptions = {
   crypt: {
@@ -21,20 +12,4 @@ const clientOptions = {
   },
 };
 
-const guacServer = new GuacamoleLite({ server }, guacdOptions, clientOptions);
-
-server.listen(HTTP_PORT, () => {
-  console.log(`HTTP server running on port ${HTTP_PORT}`);
-});
-
-guacServer.on("open", (connection) => {
-  console.log("Guacamole client connected");
-});
-
-guacServer.on("close", (connection) => {
-  console.log("Guacamole client disconnected");
-});
-
-guacServer.on("error", (clientConnection, error) => {
-  console.error(clientConnection, error);
-});
+new GuacamoleLite(websocketOptions, guacdOptions, clientOptions);
